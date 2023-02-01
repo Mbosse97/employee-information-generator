3857d40e0b60e4d9ecd030ff27db61bd9f57a0f8
Michael B <bosse730@gmail.com>
2023-02-01 11:39:29 +1100

    added file structure

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create file structure 
+// create system architecture of classes and subclass extensions 
+// import inguirer and jest
+// create prompts for employee information for entire team (only exit when no more employees to add)
+// generate HTML file based on employee information
+// create styling for generated html
+// create tests for value passing within classes and for the operations performed
